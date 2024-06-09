8046c019090cd17ce1e1f3602ada920aff747efa
KingKaiZhuang <5b0g0024@stust.edu.tw>
2024-06-09 11:59:27 +0800

    Finished Gatsby Tutorial Part 2

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,11 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+
 module.exports = {
   siteMetadata: {
     title: `My First Gatsby Site`,
     siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [],
-}
+};
